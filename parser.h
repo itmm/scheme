f842425f18650571b500cbe008d51d11765d463f
Timm Knape <timm@knp.de>
2022-03-20 13:35:08 +0100

    allow brackets for grouping

--- parser.h
+++ parser.h
@@ -16,25 +16,29 @@ void eat_space(std::istream &in) {
 	}
 }
 
-Element *read_list(std::istream &in) {
+Element *read_list(std::istream &in, int closing) {
 	eat_space(in);
 	if (ch == EOF) {
 		return err("read_list", "incomplete_list");
 	}
-	if (ch == ')') {
+	if (ch == closing) {
 		ch = in.get();
 		return nullptr;
 	}
+	if (ch == ')' || ch == ']') {
+		return err("read_list", "unmatched closing");
+	}
+
 	auto exp { read_expression(in) };
 	auto sym { dynamic_cast<Symbol *>(exp) };
 	if (sym && sym->value() == ".") {
 		auto result { read_expression(in) };
 		eat_space(in);
-		ASSERT(ch == ')', "read_list");
+		ASSERT(ch == closing, "read_list");
 		ch = in.get();
 		return result;
 	}
-	return new Pair { exp, read_list(in) };
+	return new Pair { exp, read_list(in, closing) };
 }
 
 double float_value(const std::string &v) {
@@ -46,7 +50,8 @@ double float_value(const std::string &v) {
 Element *read_expression(std::istream &in) {
 	eat_space(in);
 	if (ch == EOF) { return nullptr; }
-	if (ch == '(') { ch = in.get(); return read_list(in); }
+	if (ch == '(') { ch = in.get(); return read_list(in, ')'); }
+	if (ch == '[') { ch = in.get(); return read_list(in, ']'); }
 	if (ch == '\'') {
 		ch = in.get();
 		return new Pair {
@@ -88,7 +93,7 @@ Element *read_expression(std::istream &in) {
 		first = false;
 		result << static_cast<char>(ch);
 		ch = in.get();
-		if (ch == EOF || ch <= ' ' || ch == '(' || ch == ')') { break; }
+		if (ch == EOF || ch <= ' ' || ch == '(' || ch == ')' || ch == '[' || ch == ']' || ch == '"') { break; }
 	}
 	if (numeric && digits && ! dots) { return Integer::create(result.str()); }
 	if (numeric && digits && dots) {
