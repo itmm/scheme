f448d439abea9d38ea627d458260b330b1778fe5
Timm Knape <timm.knape@mechatronic.de>
2022-04-04 14:11:49 +0200

    refactoring operators

--- scheme.scm
+++ scheme.scm
@@ -1,22 +1,20 @@
 (define nil ())
-(define (cadr l)
-  (car (cdr l)))
-(define (cddr l)
-  (cdr (cdr l)))
-(define (caddr l)
-  (car (cddr l)))
-(define (cdddr l)
-  (cdr (cddr l)))
+(define (cadr l) (car (cdr l)))
+(define (cddr l) (cdr (cdr l)))
+(define (caddr l) (car (cddr l)))
+(define (cdddr l) (cdr (cddr l)))
 (define (list . l) l)
 (define true #t)
 (define false #f)
 (define (null? a) (eq? a '()))
+
 (define (@numeric-cascade op)
   (let ([fn #f])
     (set! fn (case-lambda
                [(a b) (op a b)]
                [(a b c . r) (and (op a b) (apply fn b c r))]))
     fn))
+
 (define (@binary> a b) (@binary< b a))
 (define (@binary<= a b) (not (@binary> a b)))
 (define (@binary>= a b) (not (@binary< a b)))
@@ -32,26 +30,19 @@
 (define (even? x) (= (remainder x 2) 0))
 (define (odd? x) (not (even? x)))
 
-(define + (case-lambda
-	   (() 0)
-	   ((a) a)
-	   ((a b) (@binary+ a b))
-	   (x (apply + (@binary+ (car x) (cadr x)) (cddr x)))))
-(define - (case-lambda
-	   (() 0)
-	   ((a) (@binary- 0 a))
-	   ((a b) (@binary- a b))
-	   (x (apply - (@binary- (car x) (cadr x)) (cddr x)))))
-(define * (case-lambda
-	   (() 1)
-	   ((a) a)
-	   ((a b) (@binary* a b))
-	   (x (apply * (@binary* (car x) (cadr x)) (cddr x)))))
-(define / (case-lambda
-	   (() 1)
-	   ((a) (@binary/ 1 a))
-	   ((a b) (@binary/ a b))
-	   (x (apply / (@binary/ (car x) (cadr x)) (cddr x)))))
+(define (@numeric-op op zero)
+   (let ([fn #f])
+     (set! fn (case-lambda
+	        [() zero]
+		[(a) (op zero a)]
+		[(a b) (op a b)]
+		[(a b c . r) (apply fn (op a b) c r)]
+	      ))))
+
+(define + (@numeric-op @binary+ 0))
+(define - (@numeric-op @binary- 0))
+(define * (@numeric-op @binary* 1))
+(define / (@numeric-op @binary/ 1))
 
 (define (map f ls . more)
   (if (null? more)
